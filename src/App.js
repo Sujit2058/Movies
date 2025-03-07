dca8bcdf9f1cf4f253997f5c853cc479a033011f
Sujit2058 <118837294+Sujit2058@users.noreply.github.com>
2025-03-07 12:27:36 +0545

    New s

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import MovieCard from './Components/MovieCard';
 
 
 function App() {
+  const movieNumber = 1;
+
+
   return (
     <>
-    <MovieCard movie={{title:"Littile Krishna", release_date:"2024"}} />
-    <MovieCard movie={{title:"Littile Krishna", release_date:"2024"}} />
+      {movieNumber === 1 ? 
+      (<MovieCard movie={{ title: "Littile Krishna", release_date: "2024" }} />) 
+      : 
+      (<MovieCard movie={{ title: "Krishna Aur Balaram", release_date: "2024" }} />)
+      }
     </>
   );
 }
